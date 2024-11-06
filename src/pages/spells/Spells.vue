a7816a4a15868b24fd38a176f59fbe45b94aaed1
Fedor <lagutinfedya@gmail.com>
2024-11-06 14:34:49 +0300

    feat: some fixes in Spells page

--- src/pages/spells/Spells.vue
+++ src/pages/spells/Spells.vue
@@ -1,114 +1,79 @@
 <script>
-import { defineComponent } from "vue";
-import debounce from "lodash/debounce.js";
+import {defineComponent} from "vue";
 import {allSpells} from "@/assets/allSpells.js";
-import axiosAgregator from "@/server/axiosAgregator.js";
 
 export default defineComponent({
   data() {
-    const items = allSpells;
+    const items = allSpells.map((e) => {
+      return e.ru;
+    });
 
     const columns = [
-      { key: "ru.level", label: "Уровень", sortable: true },
-      { key: "ru.name", label: "Название", sortable: true },
-      { key: "ru.school", label: "Школа", sortable: true },
-      { key: "ru.range", label: "Дистанция", sortable: true },
-      { key: "ru.castingTime", label: "Время каста", sortable: true },
+      {key: "level", label: "Уровень", sortable: true},
+      {key: "name", label: "Название", sortable: true},
+      {key: "school", label: "Школа", sortable: true},
+      {key: "range", label: "Дальность", sortable: true},
+      {key: "actions", label: "Допольнительная информация", width: 80},
     ];
-
-    const input = "";
 
     return {
       items,
-      columns,
-      input,
-      filter: input,
-      isDebounceInput: false,
-      isCustomFilteringFn: false,
-      filteredCount: items.length,
+      columns
     };
-  },
-
-  props: {
-    page: {
-      type: Number,
-      required: true,
-    },
-    perPage: {
-      type: Number,
-      required: true,
-    }
-  },
-
-  computed: {
-    customFilteringFn() {
-      return this.isCustomFilteringFn ? this.filterExact : undefined;
-    },
-  },
-
-  methods: {
-    loadData(){
-      const page = this.page ?? 1;
-      const perPage = this.perPage ?? 10;
-      axiosAgregator.sendGet(`/api/spells?page=${page}&perPage=${perPage}`).then(response => {
-        this.items = response.data.data;
-      })
-    },
-    filterExact(source) {
-      if (this.filter === "") {
-        return true;
-      }
-      return source?.toString?.() === this.filter;
-    },
-
-    updateFilter(filter) {
-      this.filter = filter;
-    },
-
-    debouncedUpdateFilter: debounce(function (filter) {
-      this.updateFilter(filter);
-    }, 600),
-  },
-
-  watch: {
-    input(newValue) {
-      if (this.isDebounceInput) {
-        this.debouncedUpdateFilter(newValue);
-      } else {
-        this.updateFilter(newValue);
-      }
-    },
   },
 });
 </script>
 
 <template>
-  <div class="grid md:grid-cols-2 gap-6 mb-6">
-    <VaInput
-        v-model="input"
-        placeholder="Название..."
-        class="w-full"
-    />
-  </div>
-
   <VaDataTable
       :items="items"
       :columns="columns"
-      :filter="filter"
-      :filter-method="customFilteringFn"
-      @filtered="filteredCount = $event.items.length"
-  />
-
-  <VaAlert
-      class="!mt-6"
-      color="info"
-      outline
   >
-    Number of filtered items:
-    <VaChip>{{ filteredCount }}</VaChip>
-  </VaAlert>
+    <template #cell(actions)="{ row, isExpanded }">
+      <VaButton
+          :icon="isExpanded ? 'va-arrow-up': 'va-arrow-down'"
+          preset="secondary"
+          class="w-full"
+          @click="row.toggleRowDetails()"
+      >
+        {{ isExpanded ? 'Закрыть' : 'Открыть' }}
+      </VaButton>
+    </template>
+
+    <template #expandableRow="{ rowData }">
+      <div class="flex gap-2">
+        <div class="pl-2">
+          <div class="flex items-center">
+              <span>
+                <div class="long-text">
+                  Описание: {{ rowData.text }}
+                </div>
+              </span>
+          </div>
+          <div class="flex items-center">
+            <span>Время каста: {{ rowData.castingTime }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>Мтаериалы: {{ rowData.materials }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>Компоненты: {{ rowData.components }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>Длиительность: {{ rowData.duration }}</span>
+          </div>
+          <div class="flex items-center">
+            <span>Источник: {{ rowData.source }}</span>
+          </div>
+        </div>
+      </div>
+    </template>
+  </VaDataTable>
 </template>
 
 <style scoped>
-
+.long-text {
+  width: 300px;
+  max-width: 300px;
+}
 </style>

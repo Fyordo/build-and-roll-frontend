6f9e6865a793dffeeabd1b02b07d1e542038be91
IDante <maxim.polyanskiy@mail.ru>
2024-11-06 19:56:01 +0300

    feature: add pusher notifications

--- src/pages/room/Room.vue
+++ src/pages/room/Room.vue
@@ -1,55 +1,112 @@
 <template>
-  <div class="room-page">
-    <h2>Список Комнат</h2>
-    <div v-if="loading">Загрузка...</div>
-    <div v-else-if="rooms.length === 0">Нет доступных комнат.</div>
-    <ul>
-      <li v-for="room in rooms" :key="room.title">
+  <div class="room-container">
+    <VaCard class="room-details-card" v-if="room">
+      <VaCardHeader>
         <h3>{{ room.title }}</h3>
         <p>Владелец: {{ room.owner.username }}</p>
-        <a :href="room.invitationLink" target="_blank">Присоединиться к комнате</a>
-      </li>
-    </ul>
+      </VaCardHeader>
+      <VaCardContent>
+        <p>Описание: {{ room.description }}</p>
+
+      </VaCardContent>
+      <VaCardActions>
+        <VaButton @click="leaveRoom">Покинуть комнату</VaButton>
+      </VaCardActions>
+    </VaCard>
+
+    <div v-else>
+      <p>Загрузка данных о комнате...</p>
+    </div>
+
+    <div v-if="errorMessage" class="error-message">
+      <p>{{ errorMessage }}</p>
+    </div>
+    <transition-group name="fade" tag="div">
+      <div v-for="(notification, index) in notifications" :key="index" class="notification">
+        {{ notification }}
+      </div>
+    </transition-group>
+
   </div>
 </template>
 
+
 <script setup>
 import { ref, onMounted } from 'vue';
-import axios from 'axios';
+import { useRoute, useRouter } from 'vue-router';
 import axiosAgregator from "@/server/axiosAgregator.js";
+import Pusher from "pusher-js";
 
-const rooms = ref([]);
-const loading = ref(true);
+const route = useRoute();
+const router = useRouter();
 
-const fetchRooms = async () => {
-  try {
-    console.log("sdf")
-    const response = await axiosAgregator.sendGet('/api/v1/room');
-    rooms.value = response.data;
-  } catch (error) {
-    console.error('Ошибка при загрузке комнат:', error);
-  } finally {
-    loading.value = false;
+const room = ref(null);
+const notifications = ref([]);
+
+const loadRoomData = async (id) => {
+  const response = await axiosAgregator.sendGet(`/api/v1/room/${id}`);
+  room.value = response.data; // Присваиваем полученные данные
+};
+const enablePusher = async(id) => {
+  const pusher = new Pusher('61cebc6ceca8652470ef', {
+    cluster: 'eu',
+  });
+
+  const channel = pusher.subscribe(`room_${id}_channel`);
+  console.log(`room_${id}_channel`)
+
+  channel.bind('USER_JOINED', function(data) {
+    const user = data.user.username;
+    addNotification(`${user} подключился к комнате`);
+  });
+};
+
+const addNotification = (message) => {
+  if (notifications.value.length < 3) { // Ограничение до 3 уведомлений
+    notifications.value.push(message); // Добавляем сообщение в массив уведомлений
+    setTimeout(() => {
+      notifications.value.shift(); // Удаляем первое уведомление через 10 секунд
+    }, 5000);
   }
 };
 
+
+
 onMounted(() => {
-  fetchRooms();
+  const roomId = route.params.id;
+  enablePusher(roomId);
+  loadRoomData(roomId);
 });
+
+const leaveRoom = () => {
+  router.push('/room');
+};
 </script>
 
 <style scoped>
-.room-page {
-  margin: 20px;
+.room-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 }
-.room-page h2 {
-  font-size: 24px;
+
+.room-details-card {
+  width: 100%;
+  max-width: 600px;
 }
-.room-page ul {
-  list-style-type: none;
-  padding: 0;
+.notification {
+  background-color: #255fc6; /* Цвет фона уведомления */
+  border: 1px solid #007bff; /* Граница */
+  padding: 10px; /* Отступы */
+  margin: 10px; /* Отступы между уведомлениями */
 }
-.room-page li {
-  margin-bottom: 15px;
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s ease; /* Плавный переход */
 }
+
+.fade-enter, .fade-leave-to /* .fade-leave-active в версии Vue >=2.1.8 */ {
+  opacity: 0; /* Начальная непрозрачность */
+}
+
 </style>

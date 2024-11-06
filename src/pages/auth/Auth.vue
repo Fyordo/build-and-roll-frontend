bed8b2cb07239dcce4540df3082a252107c048c2
IDante <maxim.polyanskiy@mail.ru>
2024-11-07 01:13:36 +0300

    feature: add register page

--- src/pages/auth/Auth.vue
+++ src/pages/auth/Auth.vue
@@ -12,17 +12,32 @@ const router = useRouter();
 
 const handleLogin = async () => {
   errorMessage.value = ''; // Сброс сообщения об ошибке
+  localStorage.clear();
+
   const response = await axiosAgregator.sendPost("/api/v1/auth/login", {
-    username: "sdf",
-    password: "dsf",
+    username: email.value,
+    password: password.value,
   });
 
   if (response.status === 200 && response.data.accessToken) {
     localStorage.setItem("access_token", response.data.accessToken);
     localStorage.setItem("refresh_token", response.data.refreshToken);
+    await loadUserProfile();
     router.push('/').then(() => {
       location.reload();
     });
+  } else {
+    alert("Сервер тупит");
+  }
+};
+
+const loadUserProfile = async () => {
+  const response = await axiosAgregator.sendGet("/api/v1/profile");
+
+  if (response.status === 200 && response.data.username) {
+    //console.log( response.data)
+    localStorage.setItem('username', response.data.username);
+    localStorage.setItem('userId', response.data.id);
   } else {
     alert("Сервер тупит");
   }
@@ -40,6 +55,8 @@ const handleLogin = async () => {
 
     <div v-if="errorMessage" class="error-message">{{ errorMessage }}</div>
   </div>
+
+  <p class="register-link">Нет аккаунта? <router-link to="/register">Зарегистрируйтесь</router-link></p>
 </template>
 
 <style scoped>
@@ -52,5 +69,10 @@ const handleLogin = async () => {
 .error-message {
   color: red;
   margin-top: 10px;
+}
+
+.register-link {
+  margin-top: 20px; /* Добавьте отступ сверху для лучшего визуального восприятия */
+  text-align: center; /* Центрируем текст */
 }
 </style>

6b7505f35845867f8d0cc9c4fe95cdf28fd8f61d
IDante <maxim.polyanskiy@mail.ru>
2024-11-06 17:33:40 +0300

    feature: implement room

--- src/pages/room/Room.vue
+++ src/pages/room/Room.vue
@@ -1,13 +1,55 @@
-<script>
-export default {
-  name: "Room"
-}
-</script>
-
 <template>
-
+  <div class="room-page">
+    <h2>Список Комнат</h2>
+    <div v-if="loading">Загрузка...</div>
+    <div v-else-if="rooms.length === 0">Нет доступных комнат.</div>
+    <ul>
+      <li v-for="room in rooms" :key="room.title">
+        <h3>{{ room.title }}</h3>
+        <p>Владелец: {{ room.owner.username }}</p>
+        <a :href="room.invitationLink" target="_blank">Присоединиться к комнате</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+import axiosAgregator from "@/server/axiosAgregator.js";
 
+const rooms = ref([]);
+const loading = ref(true);
+
+const fetchRooms = async () => {
+  try {
+    console.log("sdf")
+    const response = await axiosAgregator.sendGet('/api/v1/room');
+    rooms.value = response.data;
+  } catch (error) {
+    console.error('Ошибка при загрузке комнат:', error);
+  } finally {
+    loading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchRooms();
+});
+</script>
+
+<style scoped>
+.room-page {
+  margin: 20px;
+}
+.room-page h2 {
+  font-size: 24px;
+}
+.room-page ul {
+  list-style-type: none;
+  padding: 0;
+}
+.room-page li {
+  margin-bottom: 15px;
+}
 </style>
